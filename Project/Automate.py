187fd27695a5b637d902d23b5aeba21fcb09ca95
Hussain Ali <56768095+hussainnaqvee@users.noreply.github.com>
2022-03-09 23:09:46 +0500

    Update Automate.py

--- Project/Automate.py
+++ Project/Automate.py
@@ -4,7 +4,13 @@ from datetime import datetime, date, timedelta
 import requests
 import urllib
 def Reminders():
+    """
+    Formatted text file is required in a formatted manner.
+    Example text file can be found here: https://drive.google.com/uc?export=download&id=16H7gT0fdbbIk1N_StEH1UZ1WMdimSc9X
+    Enter Url of your text file that you should preferably uploaded on gDrive with public access
+    """
     file_url='https://drive.google.com/uc?export=download&id=16H7gT0fdbbIk1N_StEH1UZ1WMdimSc9X'
+    
     todo_list=[]
     file = urllib.request.urlopen(file_url)
     for line in file:
@@ -28,10 +34,13 @@ def Reminders():
         elif(exp_date==tomorrow):
             tomorrow_message+=' '.join(item)+"\n"
             
-   
+   """
+   In order to recieve messages on your WhatsApp Number, an api access is required from callmebot.com
+   you can signup for an api access throug api.callmebot.com/whatsapp
+   """
     if(today_message!="=>Important Deadlines/Reminders for today\n\n"):
         today_message+="_________________________________________\n"
-        url1 = f"https://api.callmebot.com/whatsapp.php?phone=+923065492015&text={today_message}&apikey=698276"
+        url1 = f"https://api.callmebot.com/whatsapp.php?phone={"Your Phone Number Here"}&text={today_message}&apikey={"Your API Key"}"
         r1 = requests.get(url1)
         print(r1)
 
@@ -39,7 +48,7 @@ def Reminders():
 
     if(tomorrow_message!="=>Important Deadlines/Reminders for tomorrow\n\n"):
         tomorrow_message+="_________________________________________\n"
-        url2 = f"https://api.callmebot.com/whatsapp.php?phone=+923065492015&text={tomorrow_message}&apikey=698276"
+        url2 = f"https://api.callmebot.com/whatsapp.php?phone{"Your Phone Number Here"}&text={tomorrow_message}&apikey={"Your API Key"}"
         r2 = requests.get(url2)
         print(r2)
     #pk.sendwhatmsg('+923005154602','hello created a bot',dt.datetime.now().hour,(dt.datetime.now().minute)+1)
